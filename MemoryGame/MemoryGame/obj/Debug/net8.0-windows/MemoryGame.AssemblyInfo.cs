b21b2e4788241adefb177cc8a89d42000e72c541
adinaion <79267944+adinaion@users.noreply.github.com>
2025-03-30 17:31:55 +0300

    RelayCommand Modified

--- MemoryGame/MemoryGame/obj/Debug/net8.0-windows/MemoryGame.AssemblyInfo.cs
+++ MemoryGame/MemoryGame/obj/Debug/net8.0-windows/MemoryGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e61966ee54ea4ab622f700fb90eb9471750b0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c796033de28d0132edcf41360ba3f56a039d0521")]
 [assembly: System.Reflection.AssemblyProductAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
